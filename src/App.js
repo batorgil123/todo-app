3b9aef4fc57ed388b1d2cd91505c21684b4e9084
IHAW <imalreadylost9@gmail.com>
2025-01-06 20:13:26 -0800

    fvds

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { v4 as uuidv4 } from "uuid";
 function App() {
   const [todo, setTodo] = useState([]);
   const [inputValue, setInputValue] = useState("");
-  const [error, setError] = useState("");
+  const [error, setError] = useState("No tasks yet. Add one above!");
   const Handleinput = (event) => {
     setInputValue(event.target.value);
   };
   const HandleAdd = () => {
     if (inputValue.length === 0) {
-      setError("Please Enter Todo Task");
+      setError("No tasks yet. Add one above!");
       return;
     } else {
       setError("");
@@ -23,39 +23,59 @@ function App() {
       if (id == i.id) i.status = "Completed";
     });
   };
-
+  const handleDelete = (id) => {
+    const newTodos = todo.filter((todo) => todo.id !== id);
+    setTodo(newTodos);
+  };
+  const handleAll = () => {
+    
+  }
+  const handleActive = () => {
+    
+  }
+  const handleCompleted = () => {
+    
+  }
   return (
     <div className="App">
-      <p className="title">To-Do list</p>
-      <input 
-        className="input-1"
-        placeholder="Add To Do"
-        value={inputValue}
-        onChange={Handleinput}
-      />
-      <button className="add-button" onClick={HandleAdd}>
-        Add
-      </button>
+      <div className="input-container">
+        <p className="title">To-Do list</p>
+        <input
+          className="input-1"
+          placeholder="Add a new task..."
+          value={inputValue}
+          onChange={Handleinput}
+        />
+        <button className="add-button" onClick={HandleAdd}>
+          Add
+        </button>
+      </div>
       <div className="buttons-container">
-        <button className="buttons">All</button>
-        <button className="buttons">Active</button>
-        <button className="buttons">Completed</button>
+        <button onClick={handleAll} className="button-ALL">All</button>
+        <button onClick={handleActive} className="button-ACTIVE">Active</button>
+        <button onClick={handleCompleted} className="button-Completed">Completed</button>
       </div>
 
-      {error.length > 1 && <div>{error}</div>}
-      
+      {error.length > 1 && <div className="error">{error}</div>}
+
       {todo.map((todo) => {
         return (
           <div className="list">
-            <input
-              className="checkboxes"
-              type="checkbox"
-              onChange={HandleCheckbox(todo.id)}
-            />
-            {todo.text}
+            <div className="list-item">
+              <input
+                className="checkboxes"
+                type="checkbox"
+                onChange={HandleCheckbox(todo.id)}
+              />
+              <div className="list-text">{todo.text}</div>
+            </div>
+            <button onClick={() => handleDelete(todo.id)} className="Delete">
+              Delete
+            </button>
           </div>
         );
       })}
+      <p className="bottom-title">Powered by Pinecone academy</p>
     </div>
   );
 }
